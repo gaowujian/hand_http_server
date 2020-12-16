5389b4d1cfc9b5d0512357cc66213c2f648fdd12
tony <gwjlshebei@gmail.com>
2020-12-16 23:22:53 +0800

    配置命令行

---
+++ bin/config.js
@@ -0,0 +1,16 @@
+const config = {
+  port: {
+    flags: "-p, --port <port_number>",
+    description: "set port for server",
+    defaultValue: 8000,
+    usage: "tony_server -p 3000",
+  },
+  directory: {
+    flags: "-d, --directory <directory>",
+    description: "set directory for server",
+    defaultValue: "'process.cwd()'",
+    usage: "tony_server -d ~/",
+  },
+};
+
+module.exports = config;
