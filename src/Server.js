28b905f775a302f2f6bcfeefdc6c38cd7fca6ffd
tony <gwjlshebei@gmail.com>
2020-12-19 23:15:02 +0800

    具备cache能力的静态资源服务

--- src/Server.js
+++ src/Server.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const mime = require("Mime");
 const chalk = require("chalk");
 const os = require("os");
+const crypto = require("crypto");
 class Server {
   constructor(options) {
     this.port = options.port;
@@ -18,10 +19,11 @@ class Server {
       urlObj.pathname = decodeURIComponent(urlObj.pathname); // decode 解码
 
       const requestUrl = path.join(this.directory, urlObj.pathname);
+      console.log(requestUrl);
       const stateObj = await fs.stat(requestUrl);
       // 如果是文件的话，requestUrl就是本地的文件路径
       if (stateObj.isFile()) {
-        this.sendFile(res, requestUrl);
+        this.sendFile(req, res, stateObj, requestUrl);
       } else {
         // 如果是目录的话需要去渲染一个列表，这个时候requestUrl就是本地的目录路径
         // 我们还需要去把当前的路径地址传进去，用于目录下链接的导航
@@ -50,13 +52,34 @@ class Server {
     res.end(content);
   }
 
-  async sendFile(res, filePath) {
-    const content = await fs.readFile(filePath);
+  async sendFile(req, res, stateObj, filePath) {
     // 根据文件后缀设置相应的content-type
-    const lastDodIndex = String(filePath).lastIndexOf(".");
-    const ext = filePath.slice(lastDodIndex);
-    res.setHeader("Content-Type", `${mime.getType(ext)};charset=utf-8`);
-    res.end(content);
+    // const lastDodIndex = String(filePath).lastIndexOf(".");
+    // const ext = filePath.slice(lastDodIndex);
+    res.setHeader("Content-Type", `${mime.getType(filePath)};charset=utf-8`);
+
+    // 使用强制缓存
+    res.setHeader("Cache-Control", "max-age=10");
+    res.setHeader("Expires", new Date(Date.now() + 10 * 1000).toUTCString());
+    //  使用协商缓存
+    const ctime = stateObj.ctime.toUTCString();
+    const fileContent = await fs.readFile(filePath);
+    const r = crypto.createHash("md5").update(fileContent).digest("base64");
+    res.setHeader("Last-Modified", ctime);
+    res.setHeader("Etag", r);
+    // 每次都会和最新的ctime进行比较
+    if (
+      (req.headers["if-modified-since"] === ctime) |
+      (req.headers["if-none-match"] === r)
+    ) {
+      res.statusCode = 304;
+      res.end();
+    } else {
+      // 在第一次访问资源的时候会走这里
+      // fs.createReadStream(filePath).pipe(res);
+      const content = await fs.readFile(filePath);
+      res.end(content);
+    }
   }
 
   start() {
